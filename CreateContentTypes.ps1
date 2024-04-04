c03fcd283c42435170f5b32294343361a3047c00
Johan Löfblad <johan.lofblad@b3.se>
2024-04-04 10:13:15 +0200

    Added new file

---
+++ CreateContentTypes.ps1
@@ -0,0 +1,7 @@
+Import-Module PnP.PowerShell
+
+Connect-PnPOnline -Url "https://m365x98556761.sharepoint.com/sites/contentTypeHub/" -LaunchBrowser -Interactive
+
+
+# Add-PnPTaxonomyField -DisplayName "My Managed Metadata Field 2" -InternalName "MyManagedMetadataField2" -TaxonomyItemId "907a9c01-b3ea-4887-8184-f91cc3be1500" -Id 6A15ADB5-DA89-4E75-993E-CB13D435D179 -Group "My Group"
+
